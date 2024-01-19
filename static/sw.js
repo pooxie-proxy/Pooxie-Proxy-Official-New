65188012191ce52359ab0e160405129d94bcd713
Pooxie Proxy Official <149209837+pooxie-proxy@users.noreply.github.com>
2024-01-19 15:46:13 -0700

    Delete static directory

--- static/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts("/contact/mathematics.js");
-importScripts("/contact/geography.js");
-importScripts("/contact/english.js");
-
-const sw = new UVServiceWorker();
-let userKey = new URL(location).searchParams.get('userkey');
-
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
